73885f0fd0aef786a31d9191c46fef1cf881c691
lanweki <anz.iniu@gmail.com>
2022-10-30 21:27:38 +0200

    Initial project structure

--- recipehub-web/src/main/java/com/recipehub/lanweki/RecipeHubServiceApplication.java
+++ recipehub-web/src/main/java/com/recipehub/lanweki/RecipeHubServiceApplication.java
@@ -1,4 +1,4 @@
-package com.recipehub.service;
+package com.recipehub.lanweki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
