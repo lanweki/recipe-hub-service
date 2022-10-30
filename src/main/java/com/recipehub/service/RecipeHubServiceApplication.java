ebd6d55d00261cfe8a96a808da8d7d6160b212f3
lanweki <anz.iniu@gmail.com>
2022-10-30 18:13:31 +0200

    Initial commit

---
+++ src/main/java/com/recipehub/service/RecipeHubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recipehub.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeHubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeHubServiceApplication.class, args);
+    }
+
+}
