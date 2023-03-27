54b7b6698c400d82f5dab483d14618da043ce72e
lanweki <anz.iniu@gmail.com>
2023-03-27 19:03:26 +0300

    Initial data in database

---
+++ database/initial_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO recipe_hub_user (username, user_password, first_name, last_name, user_role)
+values ('user1', 'password', 'Joe', 'Popov', 'USER');
+
+INSERT INTO recipe (recipe_name, recipe_category, cuisine, ingredients_description, instruction_description, description, create_date, created_by, picture_url)
+values ('Margherita','pasta_and_pizza','italian','12-inch round of pizza dough, stretched, 3 tablespoons tomato sauce, Extra-virgin olive oil, 2¾ ounces fresh mozzarella, 4 to 5 basil leaves, roughly torn.','Place a pizza stone or tiles on the middle rack of your oven and turn heat to its highest setting. Let it heat for at least an hour,
+Put the sauce in the center of the stretched dough and use the back of a spoon to spread it evenly across the surface, stopping approximately ½ inch from the edges.
+Drizzle a little olive oil over the pie. Break the cheese into large pieces and place these gently on the sauce. Scatter basil leaves over the top.
+Using a pizza peel, pick up the pie and slide it onto the heated stone or tiles in the oven. Bake until the crust is golden brown and the cheese is bubbling, approximately 4 to 8 minutes.','Pizza Margherita (more commonly known in English as Margherita pizza) is a typical Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt, and extra-virgin olive oil.','2021-10-19 10:23:54.000000',1,'margherita_pic.jpg');
+
+insert into recipe (recipe_name, recipe_category, cuisine, ingredients_description, instruction_description, description, create_date, created_by, picture_url)
+values  ('Skordalia','sauces_and_marinades','greek','5–6 medium potatoes
+5–7 cloves of garlic (according to preference)
+2 tbsps red wine vinegar
+1/2 a cup extra virgin olive oil
+1/2 a cup water (from the boiled potatoes)
+1 tbsp salt','To prepare this skordalia recipe, start by washing thoroughly the potatoes. Peel the potatoes and place them in a large saucepan. Cover with cold water and bring to the boil. Lower the heat to a gentle simmer and cook for about 30 minutes. (For this skordalia recipe the potatoes need to boil, until very tender.) Drain them in a colander and keep 1/2 a cup of the boiling water.
+In a food processor add 1/2 a cup of the boiling water, the garlic, red wine vinegar and season with salt. Blend until the garlic is diluted. Cut the potatoes in pieces, add them in the food processor and blend to combine. Pour in the olive oil a little bit at a time, whilst blending, like making mayonnaise. Blend until the oil is incorporated and the mixture is smooth and creamy. If you like your skordalia to have a mild garlicky taste, a little trick is to boil the garlic (peeled) in some milk for about 5 minutes, before using.
+When making a skordalia recipe, it is essential, that the potatoes are hot. If the potatoes are cold, then it will be difficult to combine the ingredients and the skordalia will become lumpy.
+If you don’t have a food processor to prepare this skordalia recipe, you can mash the potatoes with a fork and combine the ingredients, using a whisk.
+    Serve the skordalia as a delicious dip or side dish with some toasted bread or pita breads. Enjoy!
+
+','5–6 medium potatoes
+5–7 cloves of garlic (according to preference)
+2 tbsps red wine vinegar
+1/2 a cup extra virgin olive oil
+1/2 a cup water (from the boiled potatoes)
+1 tbsp salt','2022-12-19 00:14:17.436194',1,'skordalia_pic.jpg')
